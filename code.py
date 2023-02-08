ee682b8817309e7a115471939510dadec5fc653d
WanjiaRuan <wruan2@uw.edu>
2023-02-08 12:31:55 -0800

    Add Code File

---
+++ code.py
@@ -0,0 +1,13 @@
+# CSE 163 Project
+# Author: Xingyuan Zhao, Mariana Li Chen, Wanjia Ruan
+
+# Import Libraries
+
+# Code
+
+# Main
+def main():
+    print('Main Method')
+
+if __name__ == '__main__':
+    main()
