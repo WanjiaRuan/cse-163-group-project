da173c9f7d247cbdba2b0dab77ac91c4826b691c
WanjiaRuan <wruan2@uw.edu>
2023-02-23 12:59:30 -0800

    add datasets

--- code.py
+++ code.py
@@ -2,12 +2,24 @@
 # Author: Xingyuan Zhao, Mariana Li Chen, Wanjia Ruan
 
 # Import Libraries
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 
 # Code
+
 
 # Main
 def main():
     print('Main Method')
+    # data
+    year2019 = pd.read_csv('/Users/rbc/Desktop/python/cse-163-group-project/data/2019.csv')
+    
 
 if __name__ == '__main__':
     main()
