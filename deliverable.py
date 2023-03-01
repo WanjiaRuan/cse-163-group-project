660ec9d1e905f1a2ba54155f7ead01b6e6612da6
WanjiaRuan <wruan2@uw.edu>
2023-02-28 22:25:09 -0800

    clean/add datasets

--- deliverable.py
+++ deliverable.py
@@ -13,19 +13,45 @@ from sklearn.linear_model import LogisticRegression
 
 
 # Code
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    # Filter and rename the datasets
+    data = df[df['year'].isin(range(2018, 2022))]
+    data = data[['Country name', 'year', 'Life Ladder', 'Log GDP per capita',
+                'Social support', 'Healthy life expectancy at birth',
+                'Freedom to make life choices']]
+    data.columns = ['country', 'year', 'score', 'GDP(log)', 'social',
+                    'life expectancy', 'freedom']
+    # Manipulate data
+    data = data.dropna()
+    data = data.astype({'score': float, 'GDP(log)': float, 'social': float, 
+                        'life expectancy': float, 'freedom': float})
+    return data
+
+
+def join_data(data: pd.DataFrame, WORLD_FILE: str) -> gpd.GeoDataFrame:
+    world = gpd.read_file(WORLD_FILE)
+    world = world[['NAME', 'SUBUNIT', 'geometry']]
+    # Combine world and happiness datasets
+    world_data = data.merge(world, left_on='country', right_on='SUBUNIT', how='left')
+    world_data = world_data.dropna()
+    world_data_gpd = gpd.GeoDataFrame(world_data)
+    return world_data_gpd
 
 
 # Main
 def main():
     print('Main Method')
-    # data
-    year2019 = pd.read_csv('https://raw.githubusercontent.com/WanjiaRuan/cse-163-group-project/main/data/2019.csv')
-    year2020 = pd.read_csv('https://raw.githubusercontent.com/WanjiaRuan/cse-163-group-project/main/data/2020.csv')
-    year2021 = pd.read_csv('https://raw.githubusercontent.com/WanjiaRuan/cse-163-group-project/main/data/2021.csv')
-    year2022 = pd.read_csv('https://raw.githubusercontent.com/WanjiaRuan/cse-163-group-project/main/data/2022.csv')
-    # join data
-    data = [year2019, year2020, year2021, year2022]
-    data = pd.concat(data)
+
+    # Load data
+    df = pd.read_csv('https://raw.githubusercontent.com/WanjiaRuan/'
+                       'cse-163-group-project/main/data/data.csv')
+    data = clean_data(df)
+    print('complete clean')
+
+    # geodataframe
+    WORLD_FILE = '/Users/rbc/Desktop/python/cse-163-group-project/data/world.shp'
+    world_data = join_data(data, WORLD_FILE)
+    print('complete join')
 
 
 if __name__ == '__main__':
