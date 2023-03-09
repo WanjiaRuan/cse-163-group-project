a4bb728bfdbfaf052ab2fae4162106512c2ee428
WanjiaRuan <wruan2@uw.edu>
2023-03-09 13:59:40 -0800

    Add datasets

---
+++ testing.py
@@ -0,0 +1,21 @@
+# CSE 163 Project
+# Author: Xingyuan Zhao, Mariana Li Chen, Wanjia Ruan
+
+# import files 
+import pandas as pd
+from cse163_utils import assert_equals
+import deliverable
+
+def test_clean_data():
+
+
+# Main method 
+def main():
+    # datasets
+
+    # testing
+    test_clean_data()
+
+
+if __name__ == '__main__':
+    main()
