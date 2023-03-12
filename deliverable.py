f961283f78d7b30dabb223fafe7f08f23bb4fc5e
Mariana0522 <yq0522@uw.edu>
2023-03-11 18:41:35 -0800

    update

--- deliverable.py
+++ deliverable.py
@@ -52,7 +52,8 @@ def score_distr(df: pd.DataFrame) -> None:
         plot = sns.histplot(data=year_df, x="score", color="purple", kde=True, ax=axs[i])
         plot.set(xlabel=f"Happiness Score", title=f"Happiness Score {year}")
     plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.3, hspace=0.3)
-    fig.savefig("score_distribution.png")
+    plt.show()
+    fig.savefig("score_distribuotion.png")
 
 
 def score_plot(df: pd.DataFrame) -> None:
